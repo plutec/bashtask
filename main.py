edea1b79e048df74885f3851995148aa65f187a7
Antonio Sánchez <asanchez@hispasec.com>
2014-04-25 00:17:28 +0200

    Improved parsing arguments

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
+import argparse
 import task
 import database
 import time
 import subprocess
 
-def main():
+def insert(command, priority=None):
+    if not priority:
+        priority = 1
+
+    database.insert_task('db.db', command, priority)
+
+
+def main():    
+    parser = argparse.ArgumentParser(description='Bashtask manager.')
+    #parser.add_argument('command', type=str, nargs=1,
+    #                   help='the complete command to execute')
+    parser.add_argument('-c' , '--createdb', dest='create_db',
+                        default=False, action='store_true',
+                        help='to create empty database (Optional).')
+    parser.add_argument('-nr', '--notrun', dest='not_run', default=False,
+                       help='to prevent run daemon.', action='store_true',)
+    args = parser.parse_args()
+    
+    if args.create_db:
+        print "Removing old database..."
+        print "Creating empty database"
+        database.create_db(name='db.db')
+    if not args.not_run:
+        run()
+
+
+
+def run():
     while(True):
         task = database.load_next('db.db')
         if task:            
